4d529e2351a4142fcdbbc4664a33eefbd3d923cd
tracy <843358687@qq.com>
2020-08-09 14:30:51 +0800

    修改路径

--- dist/default.89cc48eb.js
+++ dist/default.89cc48eb.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64243" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56419" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/default.89cc48eb.js.map
+//# sourceMappingURL=default.89cc48eb.js.map
--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -231,7 +231,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64243" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56419" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -408,4 +408,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=src.e31bb0bc.js.map
